1aedccb888b6648232b09458b931871c52204e77
Askaell <andrey.b176@gmail.com>
2021-03-13 12:27:05 +0300

    tasks-4,5 are done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { ConnectedRouter } from 'connected-react-router';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import './index.css';
-import { store } from './redux/store';
+import { store, persistor, history } from './redux/store';
 
 import { App } from './components/App';
 
 ReactDom.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </BrowserRouter>,
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <PersistGate loading={null} persistor={persistor}>
+                <App />
+            </PersistGate>
+        </ConnectedRouter>
+    </Provider>,
     document.querySelector('#root')
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,33 @@
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore, applyMiddleware } from 'redux';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
-import reducers from './reducers';
+import rootReducer from './reducers';
 import middlewares from './middlewares';
 
 const initialState = {};
 
+const history = createBrowserHistory();
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    stateReconciler: autoMergeLevel2,
+    whitelist: ['chat', 'profile'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer(history));
+
 const store = createStore(
-    reducers,
+    persistedReducer,
     initialState,
-    composeWithDevTools(applyMiddleware(...middlewares))
+    composeWithDevTools(applyMiddleware(routerMiddleware(history), ...middlewares))
 );
 
-export { store };
+const persistor = persistStore(store);
+
+export { store, persistor, history };
